92eeb8546033dcc46fe762baa9197039d2fd7368
Sergiy Bondarenko <bs.kyiv@gmail.com>
2025-02-12 21:34:14 +0200

    8 commit

--- main.py
+++ main.py
@@ -10,47 +10,15 @@ def print_hi(name):
 
 
 # Press the green button in the gutter to run the script.
+
+from myClasses import Cat, ColorCat, Dog
+
 if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
 
-class Cat:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-    def info(self):
-        print(f"I am a cat. My name is {self.name}. I am {self.age} years old.")
-
-    def make_sound(self):
-        print("Meow")
-
-class ColorCat(Cat):
-    def __init__(self, name, age, color):
-        super().__init__(name, age)
-        self.color = color
-
-
-    def info(self):
-        print(f"I am a colorcat. My name is {self.name}.  I am {self.age} years old. My color is {self.color}.")
-
-    def make_sound(self):
-        print("Wow")
-        
-
-
-class Dog:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-    def info(self):
-        print(f"I am a dog. My name is {self.name}. I am {self.age} years old.")
-
-    def make_sound(self):
-        print("Bark")
 
 
 cat1 = Cat("Kitty", 2.5)
